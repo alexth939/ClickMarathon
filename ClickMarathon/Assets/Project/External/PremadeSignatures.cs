99573647caac791db17e5012dd12d44e8d0df8f1
alexth939 <41789108+alexth939@users.noreply.github.com>
2022-06-22 01:14:13 +0300

    Clean

--- ClickMarathon/Assets/Project/External/PremadeSignatures.cs
+++ ClickMarathon/Assets/Project/External/PremadeSignatures.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <summary>
+/// version 20.6.22
+/// </summary>
 
 namespace External.Signatures
 {
